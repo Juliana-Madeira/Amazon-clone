bd2b6e0fc87b5705d1d7c523e2c6169c977ef94b
Juliana Madeira <juliwood1980@gmail.com>
2022-05-24 17:43:35 -0300

    product card

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./styles/Home.css";
+import Product from "./Product";
+import './styles/Product.css'
 
 const Home = () => {
   return (
-    <div className='home'>
-        Home
+    <div className="home">
+      <div className="home__container">
+        <img
+            className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="background-amazon"
+          />
+        <div className="home__row">
+            <Product/>
+            <Product/>
+        </div>
+        <div className="home__row">
+            <Product/>
+            <Product/>
+            <Product/>
+        </div>
+        <div className="home__row">
+            <Product/>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
